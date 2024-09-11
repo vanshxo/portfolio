8cf6410e67a7e16ea80aa6fe8fc8df941b993172
vanshxo <khatrivansh43@gmail.com>
2024-09-11 15:44:13 +0530

    static

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -123,6 +123,9 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
