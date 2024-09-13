3ab73355b7a8fcb2f780181341758ed2c764fb89
vanshxo <khatrivansh43@gmail.com>
2024-09-13 16:47:35 +0530

    render yaml

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -120,10 +120,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 
-STATIC_URL = "static/"
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
+# This is where collectstatic will place the static files for serving
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# WhiteNoise Settings
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
